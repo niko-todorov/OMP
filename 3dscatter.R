b03ea4889010478dcce83c0d5febc843e0f719a3
niko-todorov <todorov@chapman.edu>
2022-12-28 00:36:56 -0800

    R viz
    visualization in R for the 10k, and 1m data points

--- 3dscatter.R
+++ 3dscatter.R
@@ -4,18 +4,16 @@ library(rgl)
 # This is to output a rgl plot in a rmarkdown document.
 setupKnitr()
 
-data = read.csv("C:/GitHub/OMP/data/cluster_output_8_threads_dataset.txt", header = FALSE, sep = " ")
+data = read.csv("C:/GitHub/OMP/data/cluster_output_16_threads_dataset.txt", header = FALSE, sep = " ")
 
 # Add a new column with color
-mycolors <- c('royalblue1', 'darkcyan', 'purple', 'yellow')
+mycolors <- c('royalblue', 'darkcyan', 'purple', 'yellow')
 data$color <- mycolors[ as.numeric(data$V4+1) ]
 
 # Plot
 plot3d( 
   x=data$`V1`, y=data$`V2`, z=data$`V3`, 
-  col = data$color, 
-  type = 's', 
-  radius = .1,
+  col = data$color,type = 's',radius = 3,
   xlab="X", ylab="Y", zlab="Z")
 
 # To display in an R Markdown document:
@@ -23,7 +21,7 @@ rglwidget()
 
 # To save to a file:
 htmlwidgets::saveWidget(rglwidget(width = 520, height = 520), 
-  file = "./3dscatter.html",
+  file = "./data/3dscatter.html",
   libdir = "libs",
   selfcontained = FALSE
 )
